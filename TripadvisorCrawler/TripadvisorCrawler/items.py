18f97b2ac92acf5b8da485d1d3b74efc67bdfcad
Agni Garcia <agni.cafeprot@gmail.com>
2022-09-08 18:55:19 -0500

    Crawl process and dataset compiled

--- TripadvisorCrawler/TripadvisorCrawler/items.py
+++ TripadvisorCrawler/TripadvisorCrawler/items.py
@@ -4,9 +4,51 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose
+from w3lib.html import replace_escape_chars, remove_tags
+from urllib.parse import urlparse
+
+def get_domain(response):
+    parsed_uri = urlparse(response)
+    domain = '{uri.netloc}'.format(uri=parsed_uri)
+    return domain
 
 
-class TripadvisorcrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class TripadvisorCrawlerItem(scrapy.Item):
+    restaurant_name = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+
+    address = scrapy.Field(
+        input_processor=MapCompose(replace_escape_chars, remove_tags)
+    )
+    prices_range = scrapy.Field()
+
+    food_category = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+    food_types = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+    rating = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+
+    opinions = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+
+    link = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+
+    extracted_date = scrapy.Field(
+        output_processor = TakeFirst()
+    )
+
+    domain = scrapy.Field(
+        input_processor=MapCompose(get_domain),
+        output_processor=TakeFirst()
+    )
+
--- TripadvisorCrawler/TripadvisorCrawler/spiders/advisorCrawler.py
+++ TripadvisorCrawler/TripadvisorCrawler/spiders/advisorCrawler.py
@@ -1,20 +1,56 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from scrapy.loader import ItemLoader
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.utils.project import get_project_settings
+from TripadvisorCrawler.items import TripadvisorCrawlerItem
+from datetime import datetime
 
 
 class AdvisorcrawlerSpider(CrawlSpider):
-    name = 'advisorCrawler'
-    allowed_domains = ['www.tripadvisor.com.mx']
-    start_urls = ['http://www.tripadvisor.com.mx/']
+    name = 'CrawlAdviser'
+    allowed_domains = ['tripadvisor.com.mx']
+    start_urls = ['https://www.tripadvisor.com.mx/Restaurants-g150800-Mexico_City_Central_Mexico_and_Gulf_Coast.html']
+
+    custom_settings = {
+        'FEED_URI': 'tripadvisor_restaurants.csv',
+        'FEED_FORMAT': 'csv',
+        'CURRENT_REQUESTS': 100,  # Requests simultaneas
+        'MEMUSAGE_LIMIT_MB': 2048,  # 2 gigas de memoria ram disponibles para no sobre cargar
+        'ROBOTSTXT_OBEY': True,
+        'SCHEDULER_PRIORITY_QUEUE': 'scrapy.pqueues.DownloaderAwarePriorityQueue',
+        'FEED_EXPORT_ENCODING': 'utf-8',
+        'DOWNLOAD_DELAY': 0.25
+    }
+
+    restaurant_details = LinkExtractor(restrict_xpaths='//div[@class="YtrWs"]//div[@class="RfBGI"]//a')
+    pagination = LinkExtractor(restrict_xpaths='//div[@class="unified pagination js_pageLinks"]/a')
+
+    rule_page = Rule(pagination, follow=True)
+    rule_restaurant_detail = Rule(restaurant_details, callback='parse_item', follow=False)
+
+
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        rule_page,
+        rule_restaurant_detail
     )
 
+
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        now = datetime.now()
+        extracted_date = now.strftime("%Y-%m-%d %H:%M:%S")
+        l = ItemLoader(item=TripadvisorCrawlerItem(), selector=response)
+        l.add_xpath('restaurant_name', '//div[@class="acKDw w O"]/h1/text()')
+        l.add_xpath('address', '//div[@class="vQlTa H3"][2]/span[1]//a/text()')
+        l.add_xpath('prices_range', '//div[@class="BMlpu"]/div[1]/div[2]/text()')
+        l.add_xpath('food_category', '//div[@class="BMlpu"]/div[2]/div[2]/text()')
+        l.add_xpath('food_types', '//div[@class="BMlpu"]/div[3]/div[2]/text()')
+        l.add_xpath('rating', '//div[@class="QEQvp"]/span[1]/text()')
+        l.add_xpath('opinions', '//div[@class="QEQvp"]/a/text()')
+        l.add_value('link', response.url)
+        l.add_value('extracted_date', extracted_date)
+        l.add_value('domain', response.url)
+
+        yield l.load_item()
